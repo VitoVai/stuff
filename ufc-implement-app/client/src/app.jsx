190970bf2046151fdf05585f001d3435d87a470c
Vitalius Vaidila <vitalius.vaidila@outlook.com>
2022-01-29 21:13:40 +0200

    feat: theme-added

--- ufc-implement-app/client/src/app.jsx
+++ ufc-implement-app/client/src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 import Navbar from './components/partials/navbar';
+import theme from './styles/theme';
 
 const App = () => (
-  <div className="App">
+  <ThemeProvider theme={theme}>
     <Navbar />
     Boilerplate
-  </div>
+  </ThemeProvider>
 );
 
 export default App;
---
+++ ufc-implement-app/client/src/styles/theme.jsx
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material';
+
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
+
+const theme = createTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+
+});
+
+export default theme;
