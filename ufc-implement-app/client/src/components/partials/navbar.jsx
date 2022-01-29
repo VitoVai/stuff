9dd3c0c71c6f1aa47a5e9de5bcc904116b7ab614
Vitalius Vaidila <vitalius.vaidila@outlook.com>
2022-01-29 22:04:10 +0200

    feat: basic appbar and theme added

---
+++ ufc-implement-app/client/src/components/partials/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+
+const Navbar = () => (
+  <AppBar>
+    <Toolbar>
+      UFC
+    </Toolbar>
+  </AppBar>
+
+);
+
+export default Navbar;
