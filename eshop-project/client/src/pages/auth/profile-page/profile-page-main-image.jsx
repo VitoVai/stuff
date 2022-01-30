65254a69e16d98ff2e5bf50e7e577f30b6a2891e
Vitalius Vaidila <vitalius.vaidila@outlook.com>
2022-01-30 20:02:09 +0200

    feat(106): file input button example

--- eshop-project/client/src/pages/auth/profile-page/profile-page-main-image.jsx
+++ eshop-project/client/src/pages/auth/profile-page/profile-page-main-image.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import { styled, Button, Box } from '@mui/material';
+import {
+  styled,
+  Button,
+  Box,
+} from '@mui/material';
 
-const StyledMainImage = styled('img')(({ theme }) => ({
+const StyledMainImage = styled('img')({
   borderRadius: '50%',
   height: '250px',
   width: '250px',
   objectFit: 'cover',
-  [theme.breakpoints.up('md')]: {
-    height: '285px',
-    width: '285px',
-  },
-  [theme.breakpoints.up('lg')]: {
-    height: '210px',
-    width: '210px',
-  },
-}));
+});
 
 const MainImage = ({ mainImg }) => (
-  <Box
-    sx={{
-      display: { xs: 'flex' },
-      flexDirection: { xs: 'column' },
-      alignItems: { xs: 'center' },
-      gap: 2,
-    }}
+  <Box sx={{
+    display: { xs: 'flex' },
+    flexDirection: { xs: 'column' },
+    alignItems: { xs: 'center' },
+    gap: 2,
+  }}
   >
     <StyledMainImage
       src={(mainImg && mainImg.src) ?? '/no-image.jpg'}
       alt="user"
     />
-    <Button variant="outlined" size="large">
-      Keisti
-    </Button>
+    <Button variant="outlined" size="large">Keisti</Button>
   </Box>
 );
 
